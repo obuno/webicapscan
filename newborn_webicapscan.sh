f79f2df3adc7e07cfce7786400f921ba2c02daaf
root <email@email.com>
2024-09-06 17:37:57 +0200

    initial commit

--- newborn_webicapscan.sh
+++ newborn_webicapscan.sh
@@ -9,8 +9,15 @@ fi
 
 # build and start a new webicapscan container
 docker image build -t webicapscan .
+
 #modifications needed below are in [ ] (brackets which shall be removed post updates for your environment)
-docker container create --name webicapscan -p [docker-host-ip]:5000:5000 --dns [dns-server-ip] -v [/path/to/the/clamav/databases]:/var/lib/clamav -it webicapscan:latest
+docker container create \
+  --name webicapscan \
+  -p [docker-host-ip]:5000:5000 \
+  --dns [dns-server-ip] \
+  -v [/path/to/the/clamav/databases]:/var/lib/clamav \
+  -it webicapscan:latest
+
 docker container start webicapscan
 docker save webicapscan:latest | gzip > webicapscan_latest.tar.gz
 
